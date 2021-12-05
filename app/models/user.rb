40b03e61c3c0b177fe01e5476edd8e5739cdb1b9
yu-M215 <yuu0609km.215@gmail.com>
2021-12-05 05:02:32 +0000

    [Add] user validations configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :name, :tellphone_number, :email, :birthday, presence: true
+  validates :name, length: { in: 1..10 }
+  validates :introduction, length: { maximum: 200 }
+  validates :tellphone_number, numericality: :only_integer
 end
