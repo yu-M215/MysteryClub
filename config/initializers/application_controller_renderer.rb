c3361374a5325453378acaab7a437c56d770b999
yu-M215 <yuu0609km.215@gmail.com>
2021-12-06 04:25:20 +0000

    [Update] new mystery post form updated

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5f9a7d4b433c6325a106ce81b0d5aa2886a6b04f24e7114b976dd855fb642b0ac5f80caf05761429d6759a748c040587a26ebfc411b64e08a0de8cb52e543338'
