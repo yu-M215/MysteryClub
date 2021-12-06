88e0c838fff4e8c61502e3854d988e5f6d9c98df
yu-M215 <yuu0609km.215@gmail.com>
2021-12-06 02:53:09 +0000

    [Update] index, show, new, create action created in mysteries_controller.rb

--- app/controllers/mysteries_controller.rb
+++ app/controllers/mysteries_controller.rb
@@ -1,15 +1,25 @@
 class MysteriesController < ApplicationController
 
   def index
+    @mysteries = Mystery.all
   end
 
   def show
+    @mystery = Mystery.find(params[:id])
   end
 
   def new
+    @mystery = Mystery.new
   end
 
   def create
+    @mystery = Mystery.new(mystery_params)
+    @mystery.user_id = current_user.id
+    if @mystery.save
+      redirect_to mystery_path(@mystery.id)
+    else
+      render index
+    end
   end
 
   def edit
@@ -19,6 +29,12 @@ class MysteriesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def mystery_params
+    params.require(:mystery).permit(:title,:discription,:image,:answer,:answer_image,:answer_discription,:defficullty_level,:is_opened)
   end
 
 end
