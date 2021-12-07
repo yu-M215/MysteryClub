8d3dd8742e6ac7f11be79e4d991abf80df1e2b74
yu-M215 <yuu0609km.215@gmail.com>
2021-12-07 05:19:10 +0000

    [Update] Mystery favorite function added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    mystery = Mystery.find(params[:id])
+    mystery = Mystery.find(params[:mystery_id])
     favorite = current_user.favorites.new(mystery_id: mystery.id)
     favorite.save
     redirect_back fallback_location: mystery_path(mystery)
   end
 
   def destroy
-    mystery = Mystery.find(params[:id])
+    mystery = Mystery.find(params[:mystery_id])
     favorite = current_user.favorites.find_by(mystery_id: mystery.id)
     favorite.destroy
     redirect_back fallback_location: mystery_path(mystery)
