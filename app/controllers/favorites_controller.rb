203176dc77ca3ac4c8079e0b112832e3a8c3a49c
yu-M215 <yuu0609km.215@gmail.com>
2021-12-07 05:39:07 +0000

    [Update] Asynchronous communication of like function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    mystery = Mystery.find(params[:mystery_id])
-    favorite = current_user.favorites.new(mystery_id: mystery.id)
+    @mystery = Mystery.find(params[:mystery_id])
+    favorite = current_user.favorites.new(mystery_id: @mystery.id)
     favorite.save
-    redirect_back fallback_location: mystery_path(mystery)
+    # redirect_back fallback_location: mystery_path(mystery)
   end
 
   def destroy
-    mystery = Mystery.find(params[:mystery_id])
-    favorite = current_user.favorites.find_by(mystery_id: mystery.id)
+    @mystery = Mystery.find(params[:mystery_id])
+    favorite = current_user.favorites.find_by(mystery_id: @mystery.id)
     favorite.destroy
-    redirect_back fallback_location: mystery_path(mystery)
+    # redirect_back fallback_location: mystery_path(mystery)
   end
 end
