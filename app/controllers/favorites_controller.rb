88283b3499d2f04de326bd51ad3b440a349d5826
yu-M215 <yuu0609km.215@gmail.com>
2021-12-07 05:02:06 +0000

    [Add] create action & destroy action created in favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
   def create
+    mystery = Mystery.find(params[:id])
+    favorite = current_user.favorites.new(mystery_id: mystery.id)
+    favorite.save
+    redirect_back fallback_location: mystery_path(mystery)
   end
 
   def destroy
+    mystery = Mystery.find(params[:id])
+    favorite = current_user.favorites.find_by(mystery_id: mystery.id)
+    favorite.destroy
+    redirect_back fallback_location: mystery_path(mystery)
   end
 end
