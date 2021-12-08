a1d6c42dc8821d3ca7ad22b159fa60d832083d58
yu-M215 <yuu0609km.215@gmail.com>
2021-12-08 09:46:14 +0000

    [Update] Routing added for unsubscribe, withdraw action

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
   end
 
   resources :users, only:[:show,:edit,:update]
+
+  # 退会機能のルーティング
+  # 退会確認画面の表示
+  get 'users/unsubscribe/' => 'users#unsubscribe', as: 'confirm_unsubscribe'
+  # ユーザーの有効ステータスを更新
+  patch 'users/withdraw' => 'users#withdraw', as: 'withdraw_user'
+  put 'users/withdraw' => 'users#withdraw'
+
 end
