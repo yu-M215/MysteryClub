2adc8b885e72b191109de622f3277636963c6fa4
yu-M215 <yuu0609km.215@gmail.com>
2021-12-08 06:32:13 +0000

    [Update] Add actions edit, update to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
+
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
     @mysteries = Mystery.where(user_id: @user.id)
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+      flash[:notice] = "プロフィールの更新に失敗しました"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:introduction,:profile_image,:birthday,:is_birthday_opened)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
