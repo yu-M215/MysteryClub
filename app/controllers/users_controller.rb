39307cb97d63cd4ba3c2f829e3b21a226ef14071
yu-M215 <yuu0609km.215@gmail.com>
2021-12-08 09:40:04 +0000

    [Add] Unsubscribe action, withdraw action created in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
     end
   end
 
+  # 退会画面を表示する
+  def unsubscribe
+    @user = User.find(current_user.id)
+  end
+
+　# ユーザーの有効ステータスをfalseに更新(論理削除)
+  def withdraw
+    @user = User.find(current_user.id)
+    @user.update(is_actived: false)
+    reset_session
+    redirect_to root_path
+  end
+
   private
 
   def user_params
