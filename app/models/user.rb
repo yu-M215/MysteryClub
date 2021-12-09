1a5069cc0f9fe748783cd519325680f8bceb674e
yu-M215 <yuu0609km.215@gmail.com>
2021-12-09 06:41:58 +0000

    [Update] Asossiaitions created for relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationships", foreign_key: "follower_id", dependent: :destroy
+  has_many :follwings, through: :relationships, source: :followed
+  has_many :reverse_relationships, class_name: "Relationshiop", foreign_key: "follwed_id", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   # バリデーション
   validates :name, :tellphone_number, :email, :birthday, presence: true
   validates :name, length: { in: 1..10 }
