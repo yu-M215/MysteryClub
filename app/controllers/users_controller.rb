066d35c3239b838313eea101e241deb608aeffb1
yu-M215 <yuu0609km.215@gmail.com>
2021-12-12 12:40:36 +0000

    [Update] users_controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!
   before_action :set_user, only:[:show,:edit,:update]
 
   def show
     @mysteries = Mystery.where(user_id: @user.id)
+
+    # DM機能(ルームがあればidを取得、なければ新規ルーム作成)
+    unless @user.id == current_user.id
+      @user_rooms=UserRoom.where(user_id: @user.id)
+      @my_rooms=UserRoom.where(user_id: current_user.id)
+      @my_rooms.each do |my_room|
+        @user_rooms.each do |user_room|
+          if my_room.room_id == user_room.room_id then
+            @is_room = true
+            @room_id = my_room.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @user_room = UserRoom.new
+      end
+    end
   end
 
   def edit
