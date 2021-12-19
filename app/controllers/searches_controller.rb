cdbfaf21c92d66e8aa11ba5d7a13a31d8f4f0afa
yu-M215 <yuu0609km.215@gmail.com>
2021-12-19 09:18:03 +0000

    [Update] Search result page layouts updated

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,17 @@ class SearchesController < ApplicationController
 
   def search
     @keyword = params[:keyword]
+    @model = params[:model]
     @mysteries = Mystery.search_for(@keyword)
     @users = User.search_for(@keyword)
+
+    # 検索結果が存在しなかった時のメッセージを格納
+    if @mysteries.blank?
+      @mystery_result = "キーワードを含む謎解きはありません。"
+    end
+
+    if @users.blank?
+      @user_result = "キーワードを含むユーザーはいません。"
+    end
   end
 end
