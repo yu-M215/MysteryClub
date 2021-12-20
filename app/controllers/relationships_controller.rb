2a80e3e29c1edee3e8baae594031cabccb144b43
yu-M215 <yuu0609km.215@gmail.com>
2021-12-20 15:59:33 +0000

    [Update] Follow button asynchronization

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,24 +1,27 @@
 class RelationshipsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :set_user
 
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
   end
 
   # フォロー一覧
   def followings
-     @user = User.find(params[:user_id])
   end
 
   # フォロワー一覧
   def followers
-     @user = User.find(params[:user_id])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
