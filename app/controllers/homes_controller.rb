53deeb391ac9771580c92dc7741d7f17142edbfa
yu-M215 <yuu0609km.215@gmail.com>
2021-12-21 08:26:56 +0000

    [Update] Top page layouts updated

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,9 @@
 class HomesController < ApplicationController
   def top
-    @mysteries = Mystery.limit(5).order('created_at DESC')
+    mystery_id = Mystery.left_joins(:favorites).group(:id).order(Arel.sql('COUNT(favorites.id) desc')).limit(3).pluck(:id)
+    @first_mystery = Mystery.find(mystery_id[0])
+    @second_mystery = Mystery.find(mystery_id[1])
+    @third_mystery = Mystery.find(mystery_id[2])
   end
 
   def about
