3a6d720feb616c5da25833d847a5b3c5197c1fa3
yu-M215 <yuu0609km.215@gmail.com>
2022-01-09 07:11:57 +0000

    [Update] Assosiation updated notification.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,11 @@
 class Notification < ApplicationRecord
+  #新着通知からデータを取得
+  default_scope -> { order(created_at: :desc) }
+
+  # アソシエーション
+  belongs_to :mystery, optional: true
+  belongs_to :comment, optional: true
+  belongs_to :chat, optional: true
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 end
